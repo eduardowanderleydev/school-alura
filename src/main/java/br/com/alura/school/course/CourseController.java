485ec7368c707bcbb03fe4dc76b03192069ec021
Eduardo Wanderley <eduardowanderleydev@gmail.com>
2021-12-17 15:24:06 -0300

    fix : setting courses return for endpoint "/courses"

--- src/main/java/br/com/alura/school/course/CourseController.java
+++ src/main/java/br/com/alura/school/course/CourseController.java
@@ -7,6 +7,7 @@ import org.springframework.web.server.ResponseStatusException;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.lang.String.format;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
@@ -22,7 +23,8 @@ class CourseController {
 
     @GetMapping("/courses")
     ResponseEntity<List<CourseResponse>> allCourses() {
-        return ResponseEntity.ok().build();
+        List<CourseResponse> courses = courseRepository.findAll().stream().map(CourseResponse::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(courses);
     }
 
     @GetMapping("/courses/{code}")
